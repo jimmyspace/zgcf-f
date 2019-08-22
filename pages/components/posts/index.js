c9dbb5af967288f357c33c8164afad213a314f8a
Jimmy <791092326@qq.com>
2019-08-22 15:38:21 +0800

    init

---
+++ pages/components/posts/index.js
@@ -0,0 +1,28 @@
+// pages/components/posts/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    posts_data: {
+      type: Array,
+      value: []
+    },
+    leng: {
+      type: Number
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+  
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
